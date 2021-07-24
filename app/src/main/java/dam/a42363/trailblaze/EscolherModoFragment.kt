48f9ba6ccec3c9e0ab31a63b42c7e1abc332ffab
Duarte Cunha <dpcunha007@gmail.com>
2021-07-24 15:54:47 +0100

    Implementado RecyclerView Amigos no EscolherModoFragment.kt

--- app/src/main/java/dam/a42363/trailblaze/EscolherModoFragment.kt
+++ app/src/main/java/dam/a42363/trailblaze/EscolherModoFragment.kt
@@ -1,5 +1,6 @@
 package dam.a42363.trailblaze
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,25 +8,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import dam.a42363.trailblaze.databinding.FragmentEscolherModoBinding
-import dam.a42363.trailblaze.databinding.FragmentExplorarBinding
+import dam.a42363.trailblaze.databinding.ItemAmigoBinding
+import dam.a42363.trailblaze.databinding.ItemAmigoConvidarBinding
+import dam.a42363.trailblaze.models.Friends
 
 
 class EscolherModoFragment : Fragment() {
 
     var _binding: FragmentEscolherModoBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var amigosListView: RecyclerView
+    private lateinit var db: FirebaseFirestore
+    private lateinit var userRef: CollectionReference
+    private lateinit var friendRef: Query
+    private lateinit var auth: FirebaseAuth
+    private lateinit var onlineId: String
     private lateinit var navController: NavController
+    private var adapter: FindFriendsFirestoreRecyclerAdapter? = null
+    private val friendsArray = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentEscolherModoBinding.inflate(inflater, container, false)
-
-
+        amigosListView = binding.amigosListView
+        auth = FirebaseAuth.getInstance()
+        onlineId = auth.currentUser!!.uid
+        db = FirebaseFirestore.getInstance()
+        friendRef = db.collection("Friends").document("FriendDocument").collection(onlineId)
+        userRef = db.collection("users")
+        displayAllFriends()
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -37,4 +60,76 @@ class EscolherModoFragment : Fragment() {
 
     }
 
+    private fun displayAllFriends() {
+        val options = FirestoreRecyclerOptions.Builder<Friends>()
+            .setQuery(friendRef, Friends::class.java).build()
+
+        adapter = FindFriendsFirestoreRecyclerAdapter(options, requireContext())
+        adapter!!.startListening()
+        amigosListView.adapter = adapter
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (adapter != null) {
+            adapter!!.stopListening()
+        }
+    }
+
+    inner class FindFriendsViewHolder(val amigoBinding: ItemAmigoConvidarBinding) :
+        RecyclerView.ViewHolder(amigoBinding.root) {
+        fun setVariables(nome: String, photoUrl: String, ctx: Context) {
+            amigoBinding.name.text = nome
+            Glide.with(ctx).load(photoUrl)
+                .into(amigoBinding.profileImage)
+        }
+    }
+
+    private inner class FindFriendsFirestoreRecyclerAdapter(
+        options: FirestoreRecyclerOptions<Friends>,
+        private val ctx: Context
+    ) :
+        FirestoreRecyclerAdapter<Friends, FindFriendsViewHolder>(options) {
+
+        override fun onBindViewHolder(
+            holder: FindFriendsViewHolder,
+            position: Int,
+            model: Friends
+        ) {
+            val usersID = snapshots.getSnapshot(position).id
+
+            userRef.document(usersID).addSnapshotListener { snapshot, _ ->
+                if (snapshot != null && snapshot.exists()) {
+                    val userName: String = snapshot.getString("nome")!!
+                    val photoUrl: String = snapshot.getString("photoUrl")!!
+                    holder.setVariables(userName, photoUrl, ctx)
+                    holder.amigoBinding.check.setOnCheckedChangeListener { buttonView, isChecked ->
+                        if (isChecked) {
+                            if (friendsArray.isEmpty())
+                                binding.start.visibility = View.VISIBLE
+                            friendsArray.add(usersID)
+                        } else {
+                            friendsArray.remove(usersID)
+                            if (friendsArray.isEmpty())
+                                binding.start.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): FindFriendsViewHolder {
+            return FindFriendsViewHolder(
+                ItemAmigoConvidarBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent, false
+                )
+            )
+        }
+    }
 }
