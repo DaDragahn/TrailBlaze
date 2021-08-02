93871a2b30f41e179a82b515a634d707b8307dec
Duarte Cunha <dpcunha007@gmail.com>
2021-08-02 18:14:42 +0100

    Editar Foto no EditarPerfil implementado.

--- app/src/main/java/dam/a42363/trailblaze/EditarPerfilFragment.kt
+++ app/src/main/java/dam/a42363/trailblaze/EditarPerfilFragment.kt
@@ -5,11 +5,12 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
@@ -19,14 +20,11 @@ import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import dam.a42363.trailblaze.databinding.FragmentEditarPerfilBinding
-import dam.a42363.trailblaze.databinding.FragmentExplorarBinding
-import android.provider.MediaStore
-
-import android.content.Intent
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 
 class EditarPerfilFragment : Fragment() {
@@ -37,31 +35,46 @@ class EditarPerfilFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
     private lateinit var storage: FirebaseStorage
-
+    private var isNewPhoto: Boolean = false
     private lateinit var navController: NavController
+    private lateinit var photoUrl: String
+    private lateinit var img: ByteArray
 
 
     @SuppressLint("LogNotTimber")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 
         _binding = FragmentEditarPerfilBinding.inflate(inflater, container, false)
 
         db = FirebaseFirestore.getInstance()
         val getImage = registerForActivityResult(
-            ActivityResultContracts.GetContent(),
-            ActivityResultCallback {
-                binding.profileImage.setImageURI(it)
-                Log.d("RecordRoute", "$it")
-            }
-        )
+            ActivityResultContracts.GetContent()
+        ) {
+            isNewPhoto = true
+            binding.profileImage.setImageURI(it)
+            val iStream: InputStream? = requireActivity().contentResolver.openInputStream(it)
+            img = iStream?.let { it1 -> getBytes(it1) }!!
+        }
         binding.alterarFoto.setOnClickListener {
             getImage.launch("image/*")
         }
         return binding.root
+    }
+
+    @Throws(IOException::class)
+    fun getBytes(inputStream: InputStream): ByteArray {
+        val byteBuffer = ByteArrayOutputStream()
+        val bufferSize = 1024
+        val buffer = ByteArray(bufferSize)
+        var len: Int
+        while (inputStream.read(buffer).also { len = it } != -1) {
+            byteBuffer.write(buffer, 0, len)
+        }
+        return byteBuffer.toByteArray()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -96,9 +109,9 @@ class EditarPerfilFragment : Fragment() {
                             name.setText(fullName?.get(0))
                             lastName.setText(fullName?.get(1))
 //                            email.setText(documentSnapshot.getString("email"))
-
-                            if (!documentSnapshot.getString("photoUrl").equals(""))
-                                Glide.with(this).load(documentSnapshot.getString("photoUrl"))
+                            photoUrl = documentSnapshot.getString("photoUrl").toString()
+                            if (photoUrl != "")
+                                Glide.with(this).load(photoUrl)
                                     .into(profileImage)
                         } else {
                             Log.d("MainActivity", "No such document")
@@ -119,25 +132,47 @@ class EditarPerfilFragment : Fragment() {
                 Toast.makeText(this.activity, "Empty credentials!!", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                storageRef.child("images/defaultPic.jpg").downloadUrl.addOnSuccessListener {
-                    Log.d("URL", it.toString())
+                if (isNewPhoto) {
+                    //val userRef = storageRef.child("user.jpg")
+
+                    val userRefImagesRef = storageRef.child("images/${user?.uid}/user.jpg")
+                    val uploadTask: UploadTask = userRefImagesRef.putBytes(img)
+                    uploadTask.addOnFailureListener {
+                        // Handle unsuccessful uploads
+                    }
+                        .addOnSuccessListener {   // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                            userRefImagesRef.downloadUrl.addOnSuccessListener {
+                                photoUrl = "$it"
+                                val updates: MutableMap<String, Any> = HashMap()
+                                updates["nome"] = "$txtFirstName $txtLastName"
+                                updates["photoUrl"] = photoUrl
+
+                                val profileUpdates = UserProfileChangeRequest.Builder()
+                                    .setDisplayName("$txtFirstName $txtLastName")
+                                    .setPhotoUri(Uri.parse(photoUrl))
+                                    .build()
+
+                                user?.updateProfile(profileUpdates)
+
+                                db.collection("users").document(user!!.uid).update(updates)
+                                navController.navigate(R.id.action_editarPerfilFragment_to_perfilFragment)
+                            }
+                        }
+                } else {
                     val updates: MutableMap<String, Any> = HashMap()
                     updates["nome"] = "$txtFirstName $txtLastName"
 //                    updates["email"] = email
-                    updates["photoUrl"] = it.toString()
+                    updates["photoUrl"] = photoUrl
 
                     val profileUpdates = UserProfileChangeRequest.Builder()
                         .setDisplayName("$txtFirstName $txtLastName")
-                        .setPhotoUri(Uri.parse(it.toString()))
+                        .setPhotoUri(Uri.parse(photoUrl))
                         .build()
 
                     user?.updateProfile(profileUpdates)
 
                     db.collection("users").document(user!!.uid).update(updates)
                     navController.navigate(R.id.action_editarPerfilFragment_to_perfilFragment)
-
-                }.addOnFailureListener {
-                    Log.d("URL", "Fail", it)
                 }
             }
         }
