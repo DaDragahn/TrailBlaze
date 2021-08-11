341d940fc3505867aa893aa68cbf587f0339a223
A42363 <htmartinho@gmail.com>
2021-08-11 17:39:37 +0100

    GrupoFragment.kt segunda implementação

--- app/src/main/java/dam/a42363/trailblaze/GrupoFragment.kt
+++ app/src/main/java/dam/a42363/trailblaze/GrupoFragment.kt
@@ -1,19 +1,61 @@
 package dam.a42363.trailblaze
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.net.toUri
+import androidx.navigation.NavController
+import dam.a42363.trailblaze.databinding.FragmentGrupoBinding
+import dam.a42363.trailblaze.databinding.FragmentImageShareBinding
 
 class GrupoFragment : Fragment() {
+
+    private lateinit var navController: NavController
+
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var fullImage: AppCompatImageView
+
+    var _binding: FragmentGrupoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grupo, container, false)
-    }
 
+        _binding = FragmentGrupoBinding.inflate(inflater, container, false)
+
+
+        toolbar = binding.toolbar
+        toolbar.inflateMenu(R.menu.invite_menu)
+
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.invite -> {
+
+
+                    true
+                }
+                R.id.leave -> {
+
+
+                    true
+                }
+
+                R.id.editarFoto -> {
+
+
+                    true
+                }
+
+                else -> false
+            }
+        }
+        return binding.root
+    }
 }
